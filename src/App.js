19a3f5f232eea4c5ee4c27410c3fd092c52dee8b
Atik Mahbub <atikmahbub@Atiks-MacBook-Air.local>
2021-11-03 21:16:42 +0600

    added redux tookit basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, incrementByAmount } from "./redux/reducers/counterSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Count: {counter}
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>By payload</button>
     </div>
   );
 }
